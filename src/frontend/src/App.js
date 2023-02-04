db7eafc8f9e3aedcbaa68ee0d1bc3995040e5e5c
d-tchmnt <mavroforos.nikolaos@gmail.com>
2023-02-04 22:10:39 +0200

    simple call from front end rendering simple data from backend

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 import {getAllStudents} from "./client";
+import {useEffect, useState} from 'react';
 
 function App() {
-    getAllStudents().then(res => res.json()).then(console.log);
-   return "hello friend"
+    const [students, setStudents] = useState([])
+
+    const fetchStudents = () =>
+        getAllStudents().then(res => res.json()).then(data => setStudents(data));
+
+
+    useEffect(() => {
+        console.log("component is mounted")
+        fetchStudents();
+
+    }, []);
+
+    if (students.length <= 0) {
+        return "No Data Found"
+    }
+    return students.map((student, index) => {
+        return <p key={index}>{student.id} {student.name}</p>
+    })
 }
 
 export default App;
