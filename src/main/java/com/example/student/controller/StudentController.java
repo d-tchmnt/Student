d4d060639022e55bfa400655c7eaaadc36bb6303
d-tchmnt <mavroforos.nikolaos@gmail.com>
2023-02-04 18:28:49 +0200

    Added bootstrap frontend component making rest call to backend

--- src/main/java/com/example/student/controller/StudentController.java
+++ src/main/java/com/example/student/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.example.student.controller;
 
 import com.example.student.model.GenderEnum;
 import com.example.student.model.Student;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequestMapping(path = "api/v1/students")
 public class StudentController {
 
 
     @GetMapping(path = "/all")
     public ResponseEntity<List<Student>> getAllStudents() {
+        log.info("found me!");
         List<Student> students = List.of(new Student(1L, "Nicholas Le Black", "mavroforosnicholas@gmail.com", GenderEnum.MALE), new Student(2L, "Tzi El", "karamelopoulou@saxlamara.com", GenderEnum.FEMALE));
         return new ResponseEntity<>(students, HttpStatus.OK);
     }
